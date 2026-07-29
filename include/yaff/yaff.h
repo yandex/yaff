fb431e806f32c7fd744b3809ca020bd002997e7a
GDVFox <gaaveer@gmail.com>
2026-07-29 12:06:46 +0300

    [fix] add fwd for experimental columnar bindings

--- include/yaff/yaff.h
+++ include/yaff/yaff.h
@@ -9,4 +9,10 @@ namespace yaff::exp {
 template <typename T>
 struct Sizeable;
 
-}
+template <typename C, typename R>
+void ColumnarParseTo(const C& from, R& to);
+
+template <typename C, typename R>
+::yaff::InternalOffset<C> ColumnarSerialize(::yaff::Serializer& ys, const R& from);
+
+}  // namespace yaff::exp
